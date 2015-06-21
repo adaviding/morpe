c1cc325952691252af033e61ab26a944adb31d5e
A. David Ing, PhD <ing.dave@gmail.com>
2015-06-21 09:40:19 -0700

    Added monotonic regression algorithm.
    I copied and pasted the algorithm from my Mablab/C code, and refactored
    a few variable names.  The algorithm was fully tested before.  It is
    unlikely that I broke anything.

---
+++ Csharp/MorpeSharp/MonotonicRegressionType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Morpe
+{
+	public enum MonotonicRegressionType
+	{
+		/// <summary>
+		/// A monotonic regression where the output is likely to have "flat spots".  Thus, the output is not truly monotonic.
+		/// </summary>
+		NonDecreasing,
+		/// <summary>
+		/// A monotonic regression where the flat spots of a non-decreasing function are linearly interpolated.  The output is monotonic.
+		/// </summary>
+		Increasing,
+		/// <summary>
+		/// A blending of two other methods:  NonDecreasing and Increasing.
+		/// </summary>
+		Blended
+	}
+}
